2b6113ffc2a4d1a3cb11dc6598868de8c4a22564
Marco <l.__marco@hotmail.de>
2018-09-04 15:38:51 +0200

    added class for my login panel

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-"use strict";
-const appDiv = document.getElementById('app');
-if (appDiv) {
-    appDiv.innerHTML = `
+class LoginPanel {
+    constructor() {
+        this.appDiv = document.getElementById('app');
+    }
+    setForm() {
+        this.appDiv.innerHTML = `
 <form class="form-signin mt-5">
-      <img class="mb-4" src="assets/Hochschule_Muenchen_Logo.svg" alt="" width="250" height="auto">
+  <img class="mb-4" src="assets/Hochschule_Muenchen_Logo.svg" alt="" width="250" height="auto">
+      <div class="text-center">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      </div>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
       <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+      <input type="password" id="inputPassword" class="form-control mt-1" placeholder="Password" required="">
       <div class="checkbox mb-3 mt-2">
         <label>
           <input type="checkbox" value="remember-me"> Remember me
         </label>
       </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-    </form>`;
+      <button id="submitButton" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
+`;
+    }
+    submitForm() {
+        console.log('Submitted!');
+    }
 }
+const loginPanel = new LoginPanel();
+loginPanel.setForm();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
-const appDiv: HTMLElement | null = document.getElementById('app');
+class LoginPanel {
 
-if (appDiv) {
-    appDiv.innerHTML = `
+    public appDiv: HTMLElement = document.getElementById('app');
+
+    public setForm(): void {
+        this.appDiv.innerHTML = `
 <form class="form-signin mt-5">
-      <img class="mb-4" src="assets/Hochschule_Muenchen_Logo.svg" alt="" width="250" height="auto">
+  <img class="mb-4" src="assets/Hochschule_Muenchen_Logo.svg" alt="" width="250" height="auto">
+      <div class="text-center">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      </div>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
       <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+      <input type="password" id="inputPassword" class="form-control mt-1" placeholder="Password" required="">
       <div class="checkbox mb-3 mt-2">
         <label>
           <input type="checkbox" value="remember-me"> Remember me
         </label>
       </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-    </form>`;
+      <button id="submitButton" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
+`;
+    }
+
+    public submitForm(): void {
+        console.log('Submitted!')
+    }
+
 }
+
+const loginPanel = new LoginPanel();
+loginPanel.setForm();
