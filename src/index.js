1ee946a7c5ef26cc3ec2bab62ae8ba96c9c6fd3f
Marco <l.__marco@hotmail.de>
2018-09-07 17:01:29 +0200

    simplifyed submit functionality

--- src/index.js
+++ src/index.js
@@ -2,36 +2,30 @@ class LoginPanel {
     constructor() {
         this.appDiv = document.getElementById('app');
         this.setForm();
-        let btn = document.getElementById('loginButton');
-        btn.addEventListener('click', () => this.submitForm());
+        const btn = document.getElementById('registerButton');
+        btn.addEventListener('click', (event) => {
+            event.preventDefault();
+            this.submitForm();
+        });
     }
     setForm() {
-        this.appDiv.innerHTML = `<form id="loginForm" class="form-signin mt-5">
-  <img class="mb-4" src="assets/Hochschule_Muenchen_Logo.svg" width="250" height="auto">
-      <div class="text-center">
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-      </div>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control mt-1" placeholder="Password" required="">
-      <div class="checkbox mb-3 mt-2">
-        <label>
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-      </div>
-      <button id="loginButton" class="btn btn-lg btn-primary btn-block" type="button">Sign in</button>
-    </form>
-    `;
+        this.appDiv.innerHTML = `<form id="registerForm" class="form-signin mt-5">
+    <img class="mb-4" src="./assets/Hochschule_Muenchen_Logo.svg" width="250" height="auto">
+    <div class="text-center">
+        <h1 class="h3 mb-3 font-weight-normal">Sign up</h1>
+    </div>
+    <label for="inputEmail" class="sr-only">Email address</label>
+    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+    <label for="inputPassword" class="sr-only">Password</label>
+    <input type="password" id="inputPassword" class="form-control mt-1" placeholder="Password" required="">
+    <button id="registerButton" class="btn btn-lg btn-primary btn-block mt-2 mb-3" type="click">Sign in</button>
+</form>`;
     }
     submitForm() {
-        const elementFirst = document.createElement('pre');
-        const elementSecond = document.createElement('pre');
-        elementFirst.innerHTML = 'email: ' + document.getElementById('loginForm')[0].value;
-        document.getElementById('loginForm').appendChild(elementFirst);
-        elementSecond.innerHTML = 'password: ' + document.getElementById('loginForm')[1].value;
-        document.getElementById('loginForm').appendChild(elementSecond);
+        const username = document.getElementById('registerForm')[0].value;
+        console.log(username);
     }
+    ;
 }
 new LoginPanel();
 //# sourceMappingURL=index.js.map
