1c46fb6d5b925c9cadf6640e48f0a80a169a781b
Marco <l.__marco@hotmail.de>
2018-09-11 12:25:22 +0200

    Styles are now applied via webpack now only need to integrate images via webpack

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,41 @@
-// import * as firebase from 'firebase/app';
-// import {app} from './config';
-//
-// const auth: firebase.auth.Auth = app.auth();
+import './config';
+import './styles/styles.css';
+import img from '../assets/hm-logo.svg';
+import {app} from './config';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
+const auth: firebase.auth.Auth = app.auth();
 
 class LoginPanel {
+
+    public registerPanel: string = `<form id="registerForm" class="sign-up-form mt-5">
+    <img class="mb-4" id="hm-logo" width="250" height="auto">
+    <div class="text-center">
+        <h1 class="h3 mb-3 font-weight-normal">Sign up</h1>
+    </div>
+    <label for="inputEmail" class="sr-only">Email address</label>
+    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+    <label for="inputPassword" class="sr-only">Password</label>
+    <input type="password" id="inputPassword" class="form-control mt-1" placeholder="Password" required="">
+    <button id="registerButton" type="button" class="btn btn-lg btn-primary btn-block mt-2 mb-3">Sign in</button>
+</form>`;
 
     private username: string;
     private password: string;
 
     constructor() {
+        document.body.innerHTML = this.registerPanel;
+        const image: HTMLImageElement = document.getElementById('hm-logo') as HTMLImageElement;
+        console.log(image)
+        console.log(img);
+        image.src = img;
         const btn: HTMLElement = document.getElementById('registerButton');
         btn.addEventListener('click', () => this.submitForm());
     }
 
     public submitForm(): void {
         this.setUserCredentials();
-        console.log(this.username)
     };
 
     private setUserCredentials() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: [
-        './src/config.ts',
         './src/index.ts'
     ],
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist/js')
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'js/bundle.js'
     },
     module: {
         rules: [
@@ -18,21 +19,37 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    { loader: "style-loader" },
-                    { loader: "css-loader" }
+                    {loader: "style-loader"},
+                    {loader: "css-loader"}
+                ]
+            }, {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader', options: {
+                            name: '[path][name].[ext]',
+                            outputPath: 'images/',
+                            emitFile: false
+                        }
+                    }
                 ]
             }
         ]
     },
+    devtool: 'inline-source-map',
     resolve: {
         extensions: [
             '.ts',
             '.js'
         ]
     },
-    devtool: 'inline-source-map',
     devServer: {
-        contentBase: "./dist",
-        hot: true
-    }
+        contentBase: "./dist"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Promises vs. Observables',
+            filename: 'index.html'
+        })
+    ]
 };
