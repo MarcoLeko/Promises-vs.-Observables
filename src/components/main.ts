c4dec063d75504e668425f9a8bd535170a32675a
Marco <l.__marco@hotmail.de>
2018-10-17 14:47:36 +0200

    now added a blank typescript project for plain promises and observables examples

--- src/components/main.ts
+++
@@ -1,7 +0,0 @@
-import 'bootstrap';
-import '../styles.css';
-
-import {User} from './user';
-
-const user = new User();
-window.onload = () => user.initApp();
--- src/components/user-information.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface UserInformation {
-    email: string;
-    age: number;
-    location: string;
-    todo: string[];
-}
