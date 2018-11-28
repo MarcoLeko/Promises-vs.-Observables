7fc23b153e5cfdf91752acbc404779d1827c64d6
Marco <l.__marco@hotmail.de>
2018-11-28 16:36:23 +0100

    refactored callback-section

--- src/modules/sync-vs-async.ts
+++ src/modules/sync-vs-async.ts
@@ -1,50 +1,50 @@
+
 export class Print {
 
     public msg(msg: string): void {
         document.body.innerHTML +=
-            `<div class="alert" role="alert">${msg}</div>` as string;
-    }
-
-    public time(begin: number, end: number, css: string) {
-        document.body.innerHTML += `<div class="time-box ${css}">time: ${end - begin}ms</div>`;
+            `<div class="alert" role="alert">${msg}</div>`;
     }
 }
 
 class Synchronous extends Print {
 
-    public printAndTrackTime() {
-        const begin = window.performance.now();
-        this.printMessages();
-        const end = window.performance.now();
-        this.time(begin, end, 'sync');
-    }
-
-    private printMessages(): void {
+    public printMessages(): void {
         this.msg('Hey Im message Nr. 1!');
         this.msg('Hey Im message Nr. 2 !');
         this.msg('Hey Im message Nr. 3 !');
+        this.msg('Done!');
     }
 }
 
 class Asynchronous extends Print {
 
-    public printAndTrackTime() {
-        const begin = window.performance.now();
-        this.printMessages();
-        const end = window.performance.now();
-        this.time(begin, end, 'async');
+    public printMessages(): void {
+        setTimeout(() => this.msg('Hey Im message Nr. 1 !'), Math.random() * 10);
+        setTimeout(() => this.msg('Hey Im message Nr. 2 !'), Math.random() * 10);
+        setTimeout(() => this.msg('Hey Im message Nr. 3!'), Math.random() * 10);
+        setTimeout(() => this.msg('Done!'), Math.random() * 10);
     }
 
-    private printMessages(): void {
-        this.msg('Hey Im message Nr. 1 !');
-        setTimeout(() => this.msg('Hey Im message Nr. 2 !'), 50);
-        this.msg('Hey Im message Nr. 3!');
+    public printAsyncAndSequential(): void {
+        setTimeout(() => {
+            this.msg('Hey Im message Nr. 1 !');
+            setTimeout(() => {
+                this.msg('Hey Im message Nr. 2 !');
+                setTimeout(() => {
+                    this.msg('Hey Im message Nr. 3!');
+                    setTimeout(() => {
+                        this.msg('Done!');
+                    }, Math.random() * 10);
+                }, Math.random() * 10);
+            }, Math.random() * 10);
+        }, Math.random() * 10);
     }
 }
 
 const sync = new Synchronous();
 const async = new Asynchronous();
 
-sync.printAndTrackTime();
+sync.printMessages();
 document.body.innerHTML += '<hr>';
-async.printAndTrackTime();
+async.printMessages();
