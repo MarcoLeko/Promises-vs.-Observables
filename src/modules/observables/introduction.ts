550cec27538fd5b1c289c32a82c87eea21d00749
Marco <l.__marco@hotmail.de>
2018-12-03 14:42:23 +0100

    adjusted examples from observable introduction

--- src/modules/observables/introduction.ts
+++ src/modules/observables/introduction.ts
@@ -1,7 +1,9 @@
 import {fromEvent} from 'rxjs/internal/observable/fromEvent';
+import {filter, map, reduce, take} from 'rxjs/operators';
+import * as RxJS from 'rxjs';
 
 document.body.innerHTML += `<div class="center">
-                                <input class="form-control" type="text" placeholder="Trigger event....">
+                                <input class="form-control" type="text">
                             </div>`;
 const node = document.querySelector('input[type=text]');
 
@@ -16,25 +18,26 @@ input$.subscribe({
     complete: () => console.log(`Complete!`),
 });
 
-// const source = ['1', '2', '5', 'foo', '13', '17', 'bar'];
-//
-// console.log(source);
-//
-// const result = source
-//     .map(val => parseInt(val, 10))
-//     .filter(num => !isNaN(num))
-//     .reduce((previous: number, current: number) => previous + current);
-//
-// console.log(result);
-//
-// const source$ = Rx.interval(800).pipe(
-//     take(7), map(val => source[val])
-// );
-//
-// const result$ = source$.pipe(
-//     map(val => parseInt(val, 10)),
-//     filter(num => !isNaN(num)),
-//     reduce((previous: number, current: number) => previous + current))
-//
-// source$.subscribe(res => console.log(res));
-// result$.subscribe(res => console.log(res));
+/*** Operators **/
+const source = ['1', '2', '5', 'foo', '13', '17', 'bar'];
+
+console.log(source);
+
+const result = source
+    .map(val => parseInt(val, 10))
+    .filter(num => !isNaN(num))
+    .reduce((previous: number, current: number) => previous + current);
+
+console.log(result);
+
+const source$ = RxJS.interval(800).pipe(
+    take(7), map(val => source[val])
+);
+
+const result$ = source$.pipe(
+    map(val => parseInt(val, 10)),
+    filter(num => !isNaN(num)),
+    reduce((previous: number, current: number) => previous + current));
+
+source$.subscribe(res => console.log(res));
+result$.subscribe(res => console.log(res));
