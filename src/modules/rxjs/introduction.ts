c9a24ac0c05202449c0a8a500a41c1fe1bee11fe
Marco <l.__marco@hotmail.de>
2018-12-31 14:55:30 +0100

    restructured observables introduction

--- src/modules/rxjs/introduction.ts
+++ src/modules/rxjs/introduction.ts
@@ -1,35 +1,38 @@
-import {filter, map, publish, reduce, shareReplay, take} from 'rxjs/operators';
+import {filter, map, publish, reduce, take} from 'rxjs/operators';
 import * as RxJS from 'rxjs';
 import {Observable} from 'rxjs/internal/Observable';
 import {ConnectableObservable} from 'rxjs/internal/observable/ConnectableObservable';
+import {Observer} from 'rxjs/internal/types';
 
-const alias: Observable<number> = RxJS.Observable.create((observer) => {
-    observer.next(1);
-    observer.next(2);
-    observer.next(3);
+const observer: Observer<number> = {
+    next: x => console.log('Value: ', x),
+    error: err => console.error('Error occurred: ', err),
+    complete: () => console.log('Done!')
+};
+
+const alias: Observable<number> = RxJS.Observable.create((obs) => {
+    obs.next(1);
+    obs.next(2);
+    obs.next(3);
     setTimeout(() => {
-        observer.next(4);
-        observer.complete();
+        obs.next(4);
+        obs.complete();
     }, 1000);
 });
 
 console.log('Before subscribe');
-alias.subscribe({
-    next: x => console.log('Value: ', x),
-    error: err => console.error('Error occurred: ', err),
-    complete: () => console.log('Done!'),
-});
+alias.subscribe(observer);
 console.log('After subscribe');
 
 /*** unicast vs multicast **/
-const randomNumber = new Observable<number>(observer => {
-    observer.next(Math.random());
+const randomNumber = new Observable<number>(obs => {
+    obs.next(Math.random());
 });
 
 randomNumber.subscribe(value => console.log('1st subscription emits: ', value));
 randomNumber.subscribe(value => console.log('2nd subscription emits: ', value));
 
-const multicast = randomNumber.pipe(shareReplay());
+const multicast = randomNumber.pipe(publish());
 
 multicast.subscribe(value => console.log('1st subscription emits: ', value));
 multicast.subscribe(value => console.log('2nd subscription emits: ', value));
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/modules/async-await/introduction.ts',
+    entry: './src/modules/rxjs/introduction.ts',
     devtool: 'inline-source-map',
     output: {
         filename: 'main.js',
